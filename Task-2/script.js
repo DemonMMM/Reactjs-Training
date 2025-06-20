28e9f4f844a7e5a90701abc00a6b1ea24e4daa94
Dev Methi <justfanbased@gmail.com>
2025-06-20 10:32:02 +0530

    Updated code

--- Task-2/script.js
+++ Task-2/script.js
@@ -12,29 +12,47 @@ div.appendChild(para);
 const del = document.createElement("button");
 del.innerHTML = '<i class="fa-solid fa-trash"></i>';
 del.onclick = function(){
-    div.remove();
+    const delPara = document.createElement("p");
+    delPara.innerHTML = "Are You Sure?";
+    div.replaceChild(delPara,para);
+    delYes = document.createElement("button");
+    delNo = document.createElement("button");
+    delYes.innerHTML = '<i class="fa-solid fa-check"></i>';
+    delNo.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    div.replaceChild(delYes,edit);
+    div.replaceChild(delNo,del);
+
+    delYes.onclick = function(){
+        div.remove();
+    }
+    delNo.onclick = function(){
+        div.replaceChild(para,delPara);
+        div.replaceChild(edit,delYes);
+        div.replaceChild(del,delNo);
+    }
+    // div.remove();
 }
 
 const edit = document.createElement("button");
 edit.innerHTML = '<i class="fa-solid fa-pencil"></i>';
 edit.onclick = function(){
-const form_edit = document.createElement("form");
-const input_edit = document.createElement("input");
-const submit_edit = document.createElement("input");
-input_edit.type = "text";
-submit_edit.type = "submit";
-submit_edit.value = "Edit";
-input_edit.value = inputValue;
-form_edit.appendChild(input_edit);
-form_edit.appendChild(submit_edit);
+const formEdit = document.createElement("form");
+const inputEdit = document.createElement("input");
+const submitEdit = document.createElement("input");
+inputEdit.type = "text";
+submitEdit.type = "submit";
+submitEdit.value = "Edit";
+inputEdit.value = inputValue;
+formEdit.appendChild(inputEdit);
+formEdit.appendChild(submitEdit);
 
-element.replaceChild(form_edit,div);
-form_edit.addEventListener("submit", function(event_edit){
+element.replaceChild(formEdit,div);
+formEdit.addEventListener("submit", function(event_edit){
     event_edit.preventDefault();
-    const newVal = input_edit.value;
+    const newVal = inputEdit.value;
     para.innerHTML = newVal;;
     inputValue = newVal;
-    element.replaceChild(div, form_edit);
+    element.replaceChild(div, formEdit);
 })
 }
 div.appendChild(edit);
